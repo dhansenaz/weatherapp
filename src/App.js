4b4d5e2e00fab4da3e525c1980fdf0a202c16abc
dhansenaz <dhansen@allthingsdot.com>
2019-01-05 20:18:26 -0800

    weather and form components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
+
+
+const API_KEY = 'db9471f2904cfbfc8bd11afc9540dece';
 
 class App extends Component {
+
+  getWeather = async (e) => {
+    e.preventDefault()
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <Titles />
+          <Form getWeather={this.getWeather}/>
+          <Weather />
+        </div>
       </div>
     );
   }
