aff3e763ef1bbe619de678daba743798d6c60421
dhansenaz <dhansen@allthingsdot.com>
2019-01-06 15:00:44 -0800

    rendering props from weather to App

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,13 +4,10 @@ class Weather extends React.Component {
     render() {
         return(
             <div>
-            {this.props.temperature}
-            {this.props.city}
-            {this.props.country}
-            {this.props.description}
-            {this.props.humidity}
-            
-            
+            { this.props.city && this.props.country && <p>Temperature: {this.props.temperature}</p>}
+            { this.props.location && <p>Location: {this.props.city},{this.props.country}</p>}
+            { this.props.description && <p>Conditions:{this.props.description}</p>}
+            { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
             </div>
         )
     }
