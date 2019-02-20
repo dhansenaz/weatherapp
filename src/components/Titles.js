0c44b56d35cd6a8f41c4ff25a87af3d618dfe8da
dhansenaz <dhansen@allthingsdot.com>
2019-02-20 09:10:58 -0800

    code cleanup

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,15 +1,10 @@
 import React from "react";
 
-
 const Titles = () => (
-    <div className='title'>
-    <h1>
-        Weather Finder
-    </h1>
-    <h4 className="title-desc">
-        Check Temperatures and More
-    </h4>
-</div>
-)
+  <div className="title">
+    <h1>Weather Finder</h1>
+    <h4 className="title-desc">Check Temperatures and More</h4>
+  </div>
+);
 
-export default Titles
+export default Titles;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,20 +1,23 @@
-import React from "react"
-
-
-
+import React from "react";
 
 const Weather = props => (
-    
-        <div class="weather-data">
-        { props.city && props.country && <p className="p1">The Current Temperature is: { props.temperature }</p>}
-        { props.location && <p>Location: { props.city },{ props.country }</p>}
-        { props.description && <p className="p2">The Current Conditions are: { props.description }</p>}
-        { props.humidity && <p className="p3">The Current Humidity is: { props.humidity }%</p>}
-        { props.error && <p>{ props.error }</p>}
-        </div>
+  <div class="weather-data">
+    {props.city && props.country && (
+      <p className="p1">The Current Temperature is: {props.temperature}</p>
+    )}
+    {props.location && (
+      <p>
+        Location: {props.city},{props.country}
+      </p>
+    )}
+    {props.description && (
+      <p className="p2">The Current Conditions are: {props.description}</p>
+    )}
+    {props.humidity && (
+      <p className="p3">The Current Humidity is: {props.humidity}%</p>
+    )}
+    {props.error && <p>{props.error}</p>}
+  </div>
+);
 
-    )
-
-
-
-export default Weather
+export default Weather;
